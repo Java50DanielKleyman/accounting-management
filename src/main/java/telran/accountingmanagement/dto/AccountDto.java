ee0209e2a080a6bd94c584d5b728f8168c76e879
Daniel Kleyman <denny_klm1@yahoo.com>
2024-03-27 17:05:09 +0200

    HW-82

---
+++ src/main/java/telran/accountingmanagement/dto/AccountDto.java
@@ -0,0 +1,5 @@
+package telran.accountingmanagement.dto;
+
+public record AccountDto(String email, String password, String[] roles) {
+
+}
---
+++ src/test/java/accountingmanagement/AccountingManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package accountingmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountingManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
