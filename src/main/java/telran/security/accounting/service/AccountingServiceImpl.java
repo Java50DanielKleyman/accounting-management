2bc8f6e94674a1dfa8f5335eae41b4e68f86986b
Daniel Kleyman <denny_klm1@yahoo.com>
2024-03-31 21:30:12 +0300

    HW-83

--- src/main/java/telran/security/accounting/service/AccountingServiceImpl.java
+++ src/main/java/telran/security/accounting/service/AccountingServiceImpl.java
@@ -4,9 +4,9 @@ import org.springframework.dao.DuplicateKeyException;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,9 +18,10 @@ import telran.security.accounting.model.Account;
 @Service
 @Slf4j
 @RequiredArgsConstructor
-public class AccountingServiceImpl implements AccountingService{
+public class AccountingServiceImpl implements AccountingService {
 	final MongoTemplate mongoTemplate;
 	final PasswordEncoder passwordEncoder;
+
 	@Override
 	public AccountDto addAccount(AccountDto accountDto) {
 		String email = accountDto.email();
@@ -31,29 +32,37 @@ public class AccountingServiceImpl implements AccountingService{
 		} catch (DuplicateKeyException e) {
 			throw new AccountStateException(email);
 		}
-		log.debug("account {} has been saved",email);
+		log.debug("account {} has been saved", email);
 		return account.build();
 	}
 
-	
-
 	private AccountDto getEncoded(AccountDto accountDto) {
-		
-		return new AccountDto(accountDto.email(),
-				passwordEncoder.encode(accountDto.password()), accountDto.roles());
+
+		return new AccountDto(accountDto.email(), passwordEncoder.encode(accountDto.password()), accountDto.roles());
 	}
-
-
 
 	@Override
 	public AccountDto removeAccount(String email) {
-		Account account = mongoTemplate.findAndRemove(new Query(Criteria.where("email")
-				.is(email)), Account.class);
-		if(account == null) {
+		Account account = mongoTemplate.findAndRemove(new Query(Criteria.where("email").is(email)), Account.class);
+		if (account == null) {
 			throw new AccountNotFoundException(email);
 		}
 		log.debug("account {} has been removed", email);
 		return account.build();
 	}
-	
+
+	@Override
+	public void updatePassword(String email, String newPassword) {
+		String currentUsername = SecurityContextHolder.getContext().getAuthentication().getName();
+		if (!currentUsername.equals(email)) {
+			throw new IllegalArgumentException("Only current authenticated user may update its password");
+		}
+		Account account = mongoTemplate.findById(new Query(Criteria.where("email").is(email)), Account.class);
+		if (account == null) {
+			throw new AccountNotFoundException(email);
+		}
+		AccountDto encodedAccount = getEncoded(new AccountDto(account.getEmail(), newPassword, account.getRoles()));
+		account = mongoTemplate.insert(Account.of(encodedAccount));
+	}
+
 }
